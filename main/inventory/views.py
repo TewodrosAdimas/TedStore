206b03a34d485b8fef611b202d6057fb55f5b459
TewodrosAdimas <yosefenadagim@gmail.com>
2024-10-07 18:15:56 +0300

    views  done

--- main/inventory/views.py
+++ main/inventory/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import InventoryItem
+from .serializers import InventoryItemSerializer
 
-# Create your views here.
+# List all inventory items
+class InventoryItemListView(generics.ListCreateAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
+
+# Retrieve, update, or delete a single inventory item by ID
+class InventoryItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = InventoryItem.objects.all()
+    serializer_class = InventoryItemSerializer
